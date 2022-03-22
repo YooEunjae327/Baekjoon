20d1ba2d1ae21183db66d716a3115553c327b73e
tomato <tomato6726@gmail.com>
2022-03-22 20:44:54 +0900

    input

---
+++ input.js
@@ -0,0 +1,31 @@
+// once
+
+// const readline = require("readline")
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// })
+
+// rl.on("line", function (line) {
+//   console.log(line)
+//   rl.close()
+// }).on("close", function () {
+//   process.exit()
+// })
+
+// more
+// const readline = require("readline")
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// })
+
+// let input = []
+
+// rl.on("line", function (line) {
+//   input = line.split(" ").map((el) => parseInt(el))
+//   console.log(input[0] + input[1] + input[2])
+// }).on("close", function () {
+//   process.exit()
+// })
