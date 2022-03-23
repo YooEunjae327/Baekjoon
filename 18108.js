8f54371b2d091adb5996aef5fff58d6aacbafd1b
tomato <tomato6726@gmail.com>
2022-03-23 14:58:00 +0900

    나머지
    ㆍ수학
    ㆍ구현
    ㆍ사칙연산

--- 18108.js
+++ 18108.js
@@ -1,4 +1,13 @@
 const fs = require("fs")
 let input = fs.readFileSync("./dev/stdin").toString().split(" ")
 let a = parseInt(input[0])
-console.log(a - 543)
+let b = parseInt(input[1])
+let c = parseInt(input[2])
+
+console.log(
+  "%d\n%d\n%d\n%d",
+  (a + b) % c,
+  ((a % c) + (b % c)) % c,
+  (a * b) % c,
+  ((a % c) * (b % c)) % c
+)
