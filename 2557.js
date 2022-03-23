4a871919548a172c8d43fdff7cd7920d10c7279d
tomato <tomato6726@gmail.com>
2022-03-23 14:01:47 +0900

    오븐 시계
    ㆍ수학
    ㆍ사칙연산

--- 2557.js
+++ 2557.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const fs = require("fs")
+let input = fs.readFileSync("./dev/stdin").toString().split(/ |\n/g)
+
+let a = parseInt(input[0])
+let b = parseInt(input[1])
+let c = parseInt(input[2])
+let si, bu
+
+si = a + Math.floor((b + c) / 60)
+b + c >= 60 ? (bu = b + c - Math.floor((b + c) / 60) * 60) : (bu = b + c)
+if (si >= 24) si -= 24
+
+console.log("%d %d", si, bu)
