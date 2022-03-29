f79e47eb3eb3445128c2b2d775946faa5e39057c
tomato <tomato6726@gmail.com>
2022-03-29 17:41:25 +0900

    애너그램
    ㆍ구현
    ㆍ문자열
    ㆍ정렬

---
+++ 6996.js
@@ -0,0 +1,34 @@
+// const fs = require("fs")
+// let input = require("fs")
+//   .readFileSync("./dev/stdin")
+//   .toString()
+//   .replace(/\r/g, "")
+//   .split(/ |\n/g)
+
+// let [...arr] = input.shift()
+
+// for (let i = 0; i < input.length; i += 2) {
+//   input[i].split("").sort().join("") == input[i + 1].split("").sort().join("")
+//     ? console.log(`${input[i]} & ${input[i + 1]} are anagrams.`)
+//     : console.log(`${input[i]} & ${input[i + 1]} are NOT anagrams.`)
+// }
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+let input
+
+rl.on("line", function (line) {
+  input = line
+}).on("close", function () {
+  console.log(input)
+  //   for (let i = 0; i < input.length; i += 2) {
+  //     input[i].split("").sort().join("") == input[i + 1].split("").sort().join("")
+  //       ? console.log(`${input[i]} & ${input[i + 1]} are anagrams.`)
+  //       : console.log(`${input[i]} & ${input[i + 1]} are NOT anagrams.`)
+  //   }
+  process.exit()
+})
